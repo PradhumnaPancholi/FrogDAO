a75f95df6d6453cd7d79c3bf1f5fd4464fb118ae
Zach Berwaldt <zberwaldt@tutamail.com>
2021-12-16 21:01:03 -0500

    Formatted doc, correct import, finished and compiles successfully

--- contracts/Frog.sol
+++ contracts/Frog.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contract/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
