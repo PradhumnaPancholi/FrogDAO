6609863f3f0b1ef2ac837cba58804c9e229cd068
Zach Berwaldt <zberwaldt@tutamail.com>
2021-12-18 21:32:23 -0500

    add a few tests for FrogERC30Token

--- contracts/Frog.sol
+++ contracts/Frog.sol
@@ -8,41 +8,47 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 /// @title $FROG, an erc-20 token ///
 /// @author Pradhumna Pancholi, Zach Berwaldt ///
 contract FrogERC20Token is ERC20, ERC20Permit, ERC20Votes {
+  constructor() ERC20("Frog", "FROG") ERC20Permit("Frog") {}
 
-    constructor() ERC20("Frog", "FROG") ERC20Permit("Frog") {
+  function mint(address _account, uint256 _amount) external {
+    _mint(_account, _amount);
+  }
 
-    }
+  function burn(uint256 _amount) public virtual {
+    _burn(msg.sender, _amount);
+  }
 
-    function mint(address _account, uint256 _amount) external {
-        _mint(_account, _amount);
-    }
+  function burnFrom(address _account, uint256 _amount) public virtual {
+    _burnFrom(_account, _amount);
+  }
 
-    function burn(uint256 _amount) public virtual {
-        _burn(msg.sender, _amount);
-    }
+  function _burnFrom(address _account, uint256 _amount) public virtual {
+    // come back to this.
+    uint256 decreasedAllowance = allowance(_account, msg.sender);
 
-    function burnFrom(address _account, uint256 _amount) public virtual {
-        _burnFrom(_account, _amount);
-    }
+    _approve(_account, msg.sender, decreasedAllowance);
+    _burn(_account, _amount);
+  }
 
-    function _burnFrom(address _account, uint256 _amount) public virtual {
-        // come back to this.
-        uint256 decreasedAllowance = allowance(_account, msg.sender);
+  function _mint(address _account, uint256 _amount)
+    internal
+    override(ERC20, ERC20Votes)
+  {
+    super._mint(_account, _amount);
+  }
 
-        _approve(_account, msg.sender, decreasedAllowance);
-        _burn(_account, _amount);
-    }
-    
-    function _mint(address _account, uint256 _amount) internal override(ERC20, ERC20Votes) {
-        super._mint(_account, _amount);
-    }
+  function _burn(address _account, uint256 _amount)
+    internal
+    override(ERC20, ERC20Votes)
+  {
+    super._burn(_account, _amount);
+  }
 
-    function _burn(address _account, uint256 _amount) internal override(ERC20, ERC20Votes) {
-        super._burn(_account, _amount);
-    }
-
-    function _afterTokenTransfer(address _from, address _to, uint256 _amount) internal override(ERC20, ERC20Votes){
-        super._afterTokenTransfer(_from, _to, _amount);
-    }
-
+  function _afterTokenTransfer(
+    address _from,
+    address _to,
+    uint256 _amount
+  ) internal override(ERC20, ERC20Votes) {
+    super._afterTokenTransfer(_from, _to, _amount);
+  }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,62 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("FrogERC20Token", function () {
+  it("can mint an arbirary number of tokens.", async function () {
+    const [deployer, alice, bob] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const Frog = await ethers.getContractFactory("FrogERC20Token");
+    const frog = await Frog.deploy();
+    await frog.deployed();
+    
+    const mintTx = await frog.mint(deployer.address, 1000);
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    await mintTx.wait();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const balance = await frog.balanceOf(deployer.address);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(balance.toNumber()).to.equal(1000);
   });
-});
+
+  it("can transfer tokens between two accounts", async function () {
+    const [deployer, alice, bob] = await ethers.getSigners();
+
+    const Frog = await ethers.getContractFactory("FrogERC20Token");
+    const frog = await Frog.deploy();
+    await frog.deployed();
+    // inital mint.
+    let Tx = await frog.mint(deployer.address, 1000);
+    await Tx.wait();
+
+    // transfer 50 tokens from deployer to alice.
+    Tx = await frog.transfer(alice.address, 50);
+    await Tx.wait();
+
+    const balance = await frog.balanceOf(deployer.address);
+    const aliceBalance = await frog.balanceOf(alice.address);
+
+    expect(balance.toNumber()).to.equal(950);
+    expect(aliceBalance.toNumber()).to.equal(50);
+  });
+  
+  it("can burn tokens", async function () {
+    const [deployer] = await ethers.getSigners();
+
+    const Frog = await ethers.getContractFactory("FrogERC20Token");
+    const frog = await Frog.deploy();
+    await frog.deployed();
+    // inital mint.
+    let Tx = await frog.mint(deployer.address, 1000);
+    await Tx.wait();
+
+    // transfer 50 tokens from deployer to alice.
+    Tx = await frog.burn(500);
+    await Tx.wait();
+
+    const balance = await frog.balanceOf(deployer.address);
+    const frogTotalSupply = await frog.totalSupply();
+
+    expect(balance.toNumber()).to.equal(500);
+    expect(frogTotalSupply.toNumber()).to.equal(500);
+  });
+})
