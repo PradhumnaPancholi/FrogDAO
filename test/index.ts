e977664297d4835290927b6ee50d726534deded5
Zach Berwaldt <zberwaldt@tutamail.com>
2021-12-19 14:41:42 -0500

    refactor tests

--- test/index.ts
+++ test/index.ts
@@ -1,62 +1,75 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Signer, Contract } from "ethers";
 
 describe("FrogERC20Token", function () {
-  it("can mint an arbirary number of tokens.", async function () {
-    const [deployer, alice, bob] = await ethers.getSigners();
 
-    const Frog = await ethers.getContractFactory("FrogERC20Token");
-    const frog = await Frog.deploy();
-    await frog.deployed();
-    
-    const mintTx = await frog.mint(deployer.address, 1000);
+  let Frog;
+  let frogToken: Contract;
+  let owner: any;
+  let alice: any;
+
+  this.beforeEach(async function () {
+    Frog = await ethers.getContractFactory("FrogERC20Token");
+    [owner, alice] = await ethers.getSigners();
+
+    frogToken = await Frog.deploy();
+  });
+
+  it("can mint an arbirary number of tokens.", async function () {
+    const mintTx = await frogToken.mint(owner.address, 1000);
 
     await mintTx.wait();
 
-    const balance = await frog.balanceOf(deployer.address);
+    const balance = await frogToken.balanceOf(owner.address);
 
     expect(balance.toNumber()).to.equal(1000);
   });
 
   it("can transfer tokens between two accounts", async function () {
-    const [deployer, alice, bob] = await ethers.getSigners();
+    [owner, alice] = await ethers.getSigners();
 
-    const Frog = await ethers.getContractFactory("FrogERC20Token");
-    const frog = await Frog.deploy();
-    await frog.deployed();
     // inital mint.
-    let Tx = await frog.mint(deployer.address, 1000);
+    let Tx = await frogToken.mint(owner.address, 1000);
     await Tx.wait();
 
     // transfer 50 tokens from deployer to alice.
-    Tx = await frog.transfer(alice.address, 50);
+    Tx = await frogToken.transfer(alice.address, 50);
     await Tx.wait();
 
-    const balance = await frog.balanceOf(deployer.address);
-    const aliceBalance = await frog.balanceOf(alice.address);
+    const balance = await frogToken.balanceOf(owner.address);
+    const aliceBalance = await frogToken.balanceOf(alice.address);
 
     expect(balance.toNumber()).to.equal(950);
     expect(aliceBalance.toNumber()).to.equal(50);
   });
   
   it("can burn tokens", async function () {
-    const [deployer] = await ethers.getSigners();
+    [owner] = await ethers.getSigners();
 
-    const Frog = await ethers.getContractFactory("FrogERC20Token");
-    const frog = await Frog.deploy();
-    await frog.deployed();
     // inital mint.
-    let Tx = await frog.mint(deployer.address, 1000);
+    let Tx = await frogToken.mint(owner.address, 1000);
     await Tx.wait();
 
     // transfer 50 tokens from deployer to alice.
-    Tx = await frog.burn(500);
+    Tx = await frogToken.burn(500);
     await Tx.wait();
 
-    const balance = await frog.balanceOf(deployer.address);
-    const frogTotalSupply = await frog.totalSupply();
+    const balance = await frogToken.balanceOf(owner.address);
+    const frogTotalSupply = await frogToken.totalSupply();
 
     expect(balance.toNumber()).to.equal(500);
     expect(frogTotalSupply.toNumber()).to.equal(500);
   });
+
+  it("a user can't burn more tokens than in the total supply", async function() {
+    [owner] = await ethers.getSigners();
+
+    // inital mint.
+    let Tx = await frogToken.mint(owner.address, 1000);
+    await Tx.wait();
+
+    // Should fail.
+    await expect(frogToken.burn(1500)).to.be.revertedWith("ERC20: burn amount exceeds balance");  
+  })
 })
